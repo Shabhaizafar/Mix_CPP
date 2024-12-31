a7aae9d2c67e6aa33dcbf71b9b5738ae6346331f
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-31 17:55:28 +0530

    MultiD,structure

---
+++ Basic/MultiD.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // int arr[2][2] = {
+    //     {1,2},
+    //     //4 4
+    //     {3,4}
+    //     //4 4
+    // };
+    // cout<< "Size of Array : " <<sizeof(arr)<<endl;
+    // cout<< "Length of Array : "<<sizeof(arr)/sizeof(int)<<endl;
+    // cout<< "Total no of Rows : "<< sizeof(arr)/sizeof(arr[0])<<endl;
+    // cout << "Size of Single Row :"<<  sizeof(arr[0])<<endl;
+    // cout << "Size of Single element :"<<  sizeof(arr[0][0])<<endl;
+    // cout<< "Total no of Col : " << sizeof(arr[0])/sizeof(arr[0][0])<<endl;
+
+
+    // for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    // {
+    //     for (int j = 0; j <sizeof(arr[0])/sizeof(arr[0][0]);j++)
+    //     {
+    //         cout<< arr[i][j];
+    //     }
+    //     cout<<endl;
+    // }
+    
+
+    // 1-D 
+    // int n;
+    // cout<< "Enter N :";
+    // cin >> n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+    // cout<< "Arrya is ";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << endl;
+    // }
+    // int row,col;
+    // cout<< "Enter the No Rows :";
+    // cin >> row;
+    // cout<< "Enter the No Cols :";
+    // cin >> col;
+    // int arr[row][col];
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < col;j++)
+    //     {
+    //         cin >> arr[i][j];
+    //     }
+    // }
+    // cout<<"Array is ";
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < col;j++)
+    //     {
+    //         cout << arr[i][j];
+    //     }
+    //     cout<<endl;
+    // }
+
+    return 0;
+}
+
+
+/*
+Vector  : pre-defined
+
+Vector  :         User Defined 
+*/ 
---
+++ Basic/Structure.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+// Declare  globally
+// struct{
+//     int age,rollno;
+// }s1,s2;
+
+// struct Student{
+//     int age,rollno;
+// };
+
+// struct Student{
+//     int age,rollno;
+// }s1;
+
+// int main(){
+//     struct Student{
+//         int age,rollno;
+//     },s1;
+//     cout<< "Age of St1 : ";
+//     cin >> s1.age;
+//     cout<< "RollNo of St1 : ";
+//     cin >> s1.rollno;
+//     cout <<  s1.age << s1.rollno;
+//     return 0;
+// }
+
+/*
+Vector
+Array's 
+Structure
+*/ 
